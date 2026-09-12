d33a6f8770c47c408815fa3bb1535c0b19ff4b44
Milaadmg023 <bagerim962@gmail.com>
2026-09-12 10:30:37 +0330

    Add Next.js configuration and GitHub Actions workflow for deployment to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  // چون سایت روی username.github.io/ExoCad-Portfolio سرو میشه، نه روی ریشه دامنه
+  basePath: process.env.NODE_ENV === "production" ? "/ExoCad-Portfolio" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/ExoCad-Portfolio/" : "",
 };
 
 export default nextConfig;
